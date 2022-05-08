07d5a668c5b5c6cd211d716aa994b2d443597b5d
tskabar <86514323+tskabar@users.noreply.github.com>
2022-05-08 14:28:35 +1000

    renamed

--- no_numpy_2/__init__.py
+++ no_numpy_2/__init__.py
@@ -5,4 +5,4 @@
 # example import below, you can define it in another file and import
 # it into this module with the name 'Player':
 
-from no_numpy.player import Player
+from most_recent.player import Player
